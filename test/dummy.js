cab47ae4fe501fc91b03b621dbab6d140e591852
Alessandro Arnodo <alessandro@arnodo.net>
2015-06-19 16:19:14 +0200

    update test

--- test/dummy.js
+++ test/dummy.js
@@ -3,7 +3,7 @@ import routes from '../src/routes';
 
 describe('dummy', () => {
   it('should fail', () => {
-    expect([]).to.have.length(2);
+    expect([]).to.have.length(0);
   });
   it('should pass', () => {
     expect(42).to.equal(42);
