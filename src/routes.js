ae4ccf5fa8b3c2e8f9ea5b782be1478c6635fb6b
Alessandro Arnodo <alessandro@arnodo.net>
2015-06-21 00:07:57 +0200

    fix routes

--- src/routes.js
+++ src/routes.js
@@ -9,8 +9,7 @@ import Home from './components/Home';
 let routes = (
   <Route name="app" path="/" handler={App}>
     <DefaultRoute handler={Home} name="home"/>
-    /* another route
-    <Route name="another" path="another" handler={AnotherRoute} /> */
+     // put other routes here
   </Route>
 );
 
