12f60a475efb65e88c31848d128f3a05bb3add92
Alessandro Arnodo <alessandro@arnodo.net>
2015-06-21 17:53:50 +0200

    prod build

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,38 @@ var path = require('path');
 var webpack = require('webpack');
 var hotPort = 8000;
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var StatsPlugin = require('stats-webpack-plugin');
+
+var isDev = process.env.NODE_ENV !== 'production';
+
+var plugins = isDev ? [
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoErrorsPlugin(),
+  new ExtractTextPlugin('main.css')
+] : [
+  new ExtractTextPlugin('main.css'),
+  new webpack.optimize.UglifyJsPlugin({
+    compressor: {
+      warnings: false
+    }
+  }),
+  new StatsPlugin(path.join(__dirname, 'dist', 'webpack.stats.json'), {
+    source: false,
+    modules: false
+  })
+]
 
 module.exports = {
-  devtool: 'eval',
+  devtool: isDev ? 'eval' : 'source-map',
   entry: [
+    path.join(__dirname, 'src/main.js')
+  ].concat(isDev ? [
     'webpack-dev-server/client?http://localhost:' + hotPort,
-    'webpack/hot/dev-server',
-    './src/main.js'
-  ],
+    'webpack/hot/dev-server'
+  ] : []),
   output: {
     path: path.join(__dirname, '/dist/'),
-    filename: '[name].js',
+    filename: '[name].js', // '[name]-[hash].min.js
     publicPath: '/dist/'
   },
   module: {
@@ -31,14 +52,11 @@ module.exports = {
       loader: ExtractTextPlugin.extract('css-loader')
     }]
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+  plugins: plugins.concat([
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    new ExtractTextPlugin('main.css')
-
-  ],
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      '__DEV__': JSON.stringify(isDev || true)
+    })
+  ]),
   _hotPort: hotPort
 };
