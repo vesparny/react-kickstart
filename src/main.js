acf2f699010d60b47d8104fc74123064f3703825
Alessandro Arnodo <alessandro@arnodo.net>
2015-09-21 23:31:23 +0200

    2.0.2

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import './globals.styl';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from 'react-router';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
+// import createBrowserHistory from 'history/lib/createBrowserHistory';
+import createHashHistory from 'history/lib/createHashHistory';
 import routes from './routes';
 import attachFastClick from 'fastclick';
 
@@ -14,7 +15,9 @@ attachFastClick.attach(document.body);
 // Expose globally
 window.React = React;
 
-const history = createBrowserHistory();
+// only for GitHub pages
+// do prefer createBrowserHistory
+const history = createHashHistory();
 
 ReactDOM.render(
   <Router
