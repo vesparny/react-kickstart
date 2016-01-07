84a76f9068d5f13d65ee391ebc593202661539f6
Alessandro Arnodo <alessandro@arnodo.net>
2016-01-07 09:35:07 +0100

    fix: put fastclick back

--- src/main.js
+++ src/main.js
@@ -6,7 +6,11 @@ import ReactDOM from 'react-dom';
 // only for GitHub pages
 // do prefer import { browserHistory } from 'react-router'
 import Router, {hashHistory} from 'react-router';
+import attachFastClick from 'fastclick';
 import routes from './routes';
+
+// Remove 300ms tap delay on mobile devices
+attachFastClick.attach(document.body);
 
 // Expose globally
 window.React = React;
