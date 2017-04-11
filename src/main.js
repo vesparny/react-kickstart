800104363e3995f622242a46c1296b8f695e6a16
Alessandro Arnodo <alessandro@arnodo.net>
2017-04-11 17:02:55 +0200

    format

--- src/main.js
+++ src/main.js
@@ -1,28 +1,28 @@
-import 'sanitize.css/sanitize.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import 'sanitize.css/sanitize.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById('root')
 
 let render = () => {
-  const Root = require('./components/Root').default;
-  ReactDOM.render(<Root />, rootEl);
-};
-
-if (module.hot) {
-  const renderApp = render;
-  const renderError = err => {
-    const RedBox = require('redbox-react');
-    ReactDOM.render(<RedBox error={err} />, rootEl);
-  };
-  render = () => {
-    try {
-      renderApp();
-    } catch (err) {
-      renderError(err);
-    }
-  };
-  module.hot.accept('./components/Root', render);
+  const Root = require('./components/Root').default
+  ReactDOM.render(<Root />, rootEl)
 }
 
-render();
+if (module.hot) {
+  const renderApp = render
+  const renderError = err => {
+    const RedBox = require('redbox-react')
+    ReactDOM.render(<RedBox error={err} />, rootEl)
+  }
+  render = () => {
+    try {
+      renderApp()
+    } catch (err) {
+      renderError(err)
+    }
+  }
+  module.hot.accept('./components/Root', render)
+}
+
+render()
