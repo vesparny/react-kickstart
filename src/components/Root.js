656769aa8f7891c93e104e29ad7eb1772833baab
Alessandro Arnodo <alessandro@arnodo.net>
2017-04-11 16:54:14 +0200

    fix: lint

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,16 +1,16 @@
 // @flow
-import React from 'react'
-import { ThemeProvider } from 'glamorous'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Home from './Home'
-import theme from '../theme'
+import React from 'react';
+import { ThemeProvider } from 'glamorous';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import theme from '../theme';
 
 const Root = () => (
   <ThemeProvider theme={theme}>
     <Router>
-      <Route path='/' component={Home} />
+      <Route path="/" component={Home} />
     </Router>
   </ThemeProvider>
-)
+);
 
-export default Root
+export default Root;
